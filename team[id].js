1f840953176619061bff5cfacec4657c0a4cf95f
neokrassimir-arch <neokrassimir@gmail.com>
2025-11-07 14:06:05 -0500

    Update team[id].js

--- team[id].js
+++ team[id].js
@@ -1,13 +1,1 @@
-import fetch from 'node-fetch';
-
-export default async function handler(req, res) {
-  const { id } = req.query;
-  try {
-    const response = await fetch(`https://statsapi.mlb.com/api/v1/teams/${id}/roster`);
-    const data = await response.json();
-    res.status(200).json(data);
-  } catch (err) {
-    res.status(500).json({ error: 'Failed to fetch team roster' });
-  }
-}
 
